552e005300b478eed3f0d91a15f028e44411d256
HARSH RAJ <142962923+harshgit1406@users.noreply.github.com>
2024-11-11 09:45:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,1 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDG7LjX4ACd7Yb0oTr4IKumODjbWlKw_1M",
-    authDomain: "yumstream.firebaseapp.com",
-    projectId: "yumstream",
-    storageBucket: "yumstream.firebasestorage.app",
-    messagingSenderId: "401020524654",
-    appId: "1:401020524654:web:aaeb453e74b4c0f7a02e7f"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
